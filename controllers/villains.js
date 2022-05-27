4bb5feed8ceccd1fe8cd7efc0255efadcb4a9f94
Shane Tierney <sptierney713@gmail.com>
2022-05-27 15:48:19 -0400

    added try catch statements to all controllers that access the db

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,39 +1,51 @@
 const villains = require('../models/index')
 
 const getAllVillains = async (request, response) => {
-  const listOfTeams = await villains.findAll()
+  try {
+    const listOfTeams = await villains.findAll()
 
-  return response.status(200).send(listOfTeams)
+    return response.status(200).send(listOfTeams)
+  } catch (error) {
+    return response.sendStatus(500)
+  }
 }
 
 const getVillainBySlug = async (request, response) => {
-  const { slug } = request.params
+  try {
+    const { slug } = request.params
 
-  const foundVillain = await villains.findOne({
-    where: { slug: slug }
-  })
+    const foundVillain = await villains.findOne({
+      where: { slug: slug }
+    })
 
-  if (foundVillain) {
-    return response.status(200).send(foundVillain)
-  } else {
-    return response.status(404).send('Villain not found')
+    if (foundVillain) {
+      return response.status(200).send(foundVillain)
+    } else {
+      return response.status(404).send('Villain not found')
+    }
+  } catch (error) {
+    return response.sendStatus(500)
   }
 }
 
 const createNewVillain = async (request, response) => {
-  const { name, movie, slug } = request.body
+  try {
+    const { name, movie, slug } = request.body
 
-  if (!name || !movie || !slug) {
-    return response.status(400).send('The following fields are required: name, movie, slug')
+    if (!name || !movie || !slug) {
+      return response.status(400).send('The following fields are required: name, movie, slug')
+    }
+
+    const newVillain = await villains.create({
+      name,
+      movie,
+      slug
+    })
+
+    return response.status(201).send(newVillain)
+  } catch (error) {
+    return response.sendStatus(500)
   }
-
-  const newVillain = await villains.create({
-    name,
-    movie,
-    slug
-  })
-
-  return response.status(201).send(newVillain)
 }
 
 const isRunning = () => {
